a790c3e3f214272ac77f9198e850904fd9ef3e5d
raulito28 <raulbuenoherrera@gmail.com>
2021-11-25 18:05:08 +0100

    Update

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #Ejercicio10: repetir el ejercicio de clase 13 pidiendo la lista por teclado con un  bucle infinito hasta que se introduzca como dato fin. Los elementos de la lista han de ser float.
 import stats_data as sd
 lista = []
-numeros = float(input("Dime numeros: "))
+numeros = input("Dime numeros: ")
+numeros2 = input("Dime numeros: ")
 while True:
     numeros = float(input("Dime numeros: "))
-    if numeros == "fin":
+    numeros2 = str(input("Dime numeros: "))
+    if numeros2 == "fin":
         break
     lista.append(numeros)
+    lista.append(numeros2)
 
 res = sd.StatsData(numeros)
 print("Lista números: ")
